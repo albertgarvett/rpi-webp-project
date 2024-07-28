cd34bc3c338600ef0f495f4e83c7350c92d1434a
Croxel Inc <65045116+albertgarvett@users.noreply.github.com>
2024-07-28 16:23:20 -0400

    Adding test files

---
+++ scripts/play_audio.py
@@ -0,0 +1,15 @@
+import pygame
+import time
+
+def play_audio(file_path):
+    pygame.mixer.init()
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+
+    # Wait for the music to finish playing
+    while pygame.mixer.music.get_busy():
+        time.sleep(1)
+
+if __name__ == "__main__":
+    audio_file = "/home/pi/rpi-webp-project/webp_files/audio.mp3"
+    play_audio(audio_file)
