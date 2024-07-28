d266b5803f3852b6a2faf985bca101b2318fe95d
Croxel Inc <65045116+albertgarvett@users.noreply.github.com>
2024-07-28 16:58:51 -0400

    update bash

--- scripts/display_webp.sh
+++ scripts/display_webp.sh
@@ -4,7 +4,8 @@
 GIF_FILE="/home/pi/rpi-webp-project/webp_files/animation.gif"
 
 # Display the .gif file in full screen and loop it indefinitely
-mpv --fs --loop=inf $GIF_FILE &
+# mpv --fs --loop=inf $GIF_FILE & # to run simultaneously
+mpv --fs --loop=inf $GIF_FILE 
 
 # Play the audio file
 # python3 /home/pi/rpi-webp-project/scripts/play_audio.py &
