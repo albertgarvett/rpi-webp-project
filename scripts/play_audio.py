ddbb19aa3b86ae3ee7a8acc3982d4cc98018a24f
Croxel Inc <65045116+albertgarvett@users.noreply.github.com>
2024-07-28 16:28:45 -0400

    Presetting volume to 50%

--- scripts/play_audio.py
+++ scripts/play_audio.py
@@ -1,9 +1,10 @@
 import pygame
 import time
 
-def play_audio(file_path):
+def play_audio(file_path, volume=0.5):
     pygame.mixer.init()
     pygame.mixer.music.load(file_path)
+    pygame.mixer.music.set_volume(volume)
     pygame.mixer.music.play()
 
     # Wait for the music to finish playing
@@ -12,4 +13,4 @@ def play_audio(file_path):
 
 if __name__ == "__main__":
     audio_file = "/home/pi/rpi-webp-project/webp_files/audio.mp3"
-    play_audio(audio_file)
+    play_audio(audio_file, volume=0.5)  # Set volume to 50%
