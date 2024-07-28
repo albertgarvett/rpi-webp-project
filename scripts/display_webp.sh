d3abe56ae2594d95c5f202e0bdb7703cb66168f3
Croxel Inc <65045116+albertgarvett@users.noreply.github.com>
2024-07-27 23:08:20 -0400

    Chnage to use mpv player

--- scripts/display_webp.sh
+++ scripts/display_webp.sh
@@ -3,5 +3,5 @@
 # Path to the .webp file
 WEBP_FILE="/home/pi/rpi-webp-project/webp_files/animation.webp"
 
-# Display the .webp file in full screen
-fbi -T 1 --noverbose -a $WEBP_FILE
+# Display the .webp file in full screen and loop it indefinitely
+mpv --fs --loop=inf $WEBP_FILE
