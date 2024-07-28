53945300b70b3cd7d418f48b3280a5c2d5725163
Croxel Inc <65045116+albertgarvett@users.noreply.github.com>
2024-07-27 22:34:38 -0400

    Initial setup with display_webp.sh script

---
+++ scripts/display_webp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Path to the .webp file
+WEBP_FILE="/home/pi/rpi-webp-project/webp_files/animation.webp"
+
+# Display the .webp file in full screen
+fbi -T 1 --noverbose -a $WEBP_FILE
